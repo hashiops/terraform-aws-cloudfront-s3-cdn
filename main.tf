2312baaa2ac27a72fe281408df257a1f714bad23
aliaksei maiseyeu <>
2018-07-06 20:28:03 +0300

    modify actions

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ data "aws_iam_policy_document" "origin" {
   }
 
   statement {
-    actions   = ["s3:ListBucket"]
+    actions   = ["s3:GetObject", "s3:PutObject"]
     resources = ["arn:aws:s3:::$${bucket_name}"]
 
     principals {
